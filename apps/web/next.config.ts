4b5059b5a18160686972f9a320bd0d4b14b5fdca
manuel <radamanuelrod@gmail.com>
2025-09-17 01:46:23 +0000

    feat: Add redirects in next.config.ts to route root to login page

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+ async redirects() {
+    return [
+      {
+        source: '/', // Si la petición es para la ruta raíz
+        destination: '/login', // Redirígela a /login
+        permanent: true, // Es una redirección permanente
+      },
+    ];
+  },
 };
 
 export default nextConfig;
